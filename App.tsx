0652d5ed838cf912f0a9c032d86c54390909a64a
sousegi <sousegi@gmail.com>
2023-04-26 22:34:45 +0300

    Initial commit #1

---
+++ App.tsx
@@ -0,0 +1,109 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+    Button, Pressable,
+    StyleSheet,
+    Text, TextInput,
+    useColorScheme,
+    View,
+} from 'react-native';
+
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.signIn}>Sign In</Text>
+            <Text style={styles.forgot}>with your email and password</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Enter email"
+            />
+            <TextInput
+                style={styles.input}
+                placeholder="Enter Password"
+            />
+            <Pressable style={styles.button}>
+                <Text style={styles.login}>Log In</Text>
+            </Pressable>
+            <Text style={styles.forgot}>Forgot password?<Pressable>
+                <Text style={styles.reset}>RESET</Text>
+            </Pressable></Text>
+            <Text style={styles.create}>Create account</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#F0F3F5',
+        justifyContent: 'center',
+    },
+    signIn: {
+      fontSize: 42,
+      fontWeight: 'bold',
+      alignSelf: 'center',
+    },
+    input: {
+        height: 50,
+        marginBottom: 12,
+        margin: 10,
+        padding: 10,
+        borderRadius: 10,
+        borderWidth: 1,
+    },
+    button: {
+        height: 50,
+        marginBottom: 12,
+        margin: 10,
+        padding: 10,
+        borderRadius: 10,
+        alignItems: 'center',
+        backgroundColor: '#DB2927',
+    },
+    login: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        letterSpacing: 0.25,
+        color: 'white',
+    },
+    forgot: {
+        fontSize: 18,
+        alignSelf: 'center',
+        padding: 30,
+    },
+    reset: {
+        fontSize: 18,
+        alignSelf: 'center',
+        padding: 30,
+        color: '#1C58B2',
+    },
+    create: {
+        fontSize: 18,
+        alignSelf: 'center',
+        color: '#1C58B2',
+    },
+});
+
+export default App;
---
+++ index.js
@@ -0,0 +1,20 @@
+///**
+// * @format
+// */
+//
+//import {AppRegistry} from 'react-native';
+//import App from './App';
+//import {name as appName} from './app.json';
+//
+//AppRegistry.registerComponent(appName, () => App);
+
+
+import { AppRegistry, Platform } from 'react-native';
+import App from './App';
+
+AppRegistry.registerComponent('main', () => App);
+
+if (Platform.OS === 'web') {
+    const rootTag = document.getElementById('root') || document.getElementById('X');
+    AppRegistry.runApplication('X', { rootTag });
+}
