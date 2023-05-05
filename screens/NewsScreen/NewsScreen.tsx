ae1f518403b0b72bdc54e466b3b493cd74438d9e
sousegi <sousegi@gmail.com>
2023-05-05 16:13:40 +0300

    need icon for bottom bar #1

--- screens/NewsScreen/NewsScreen.tsx
+++ screens/NewsScreen/NewsScreen.tsx
@@ -1,104 +1,74 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import * as React from 'react';
+import {Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import type {PropsWithChildren} from 'react';
-import {
-  TouchableOpacity,
-  StyleSheet,
-  Text,
-  TextInput,
-  useColorScheme,
-  View, Button,
-} from 'react-native';
-
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-import { BottomTabBar } from "@react-navigation/bottom-tabs";
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function NewsScreen(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  function SettingsScreen() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Settings!</Text>
-      </View>
-    );
-  }
-
-
-  const Tab = createBottomTabNavigator();
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+function Feed() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.input}>this is name</Text>
-      <Text style={styles.input}>this is email</Text>
-      <Tab.Navigator>
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Feed!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F0F3F5',
-    justifyContent: 'center',
-  },
-  signIn: {
-    fontSize: 42,
-    fontWeight: 'bold',
-    alignSelf: 'center',
-  },
-  input: {
-    height: 50,
-    marginBottom: 12,
-    margin: 10,
-    padding: 10,
-    borderRadius: 10,
-    borderWidth: 1,
-  },
-  button: {
-    height: 50,
-    marginBottom: 12,
-    margin: 10,
-    padding: 10,
-    borderRadius: 10,
-    alignItems: 'center',
-    backgroundColor: '#0891b2',
-  },
-  login: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    letterSpacing: 0.25,
-    color: 'white',
-  },
-  forgot: {
-    fontSize: 18,
-    alignSelf: 'center',
-    padding: 30,
-  },
-  reset: {
-    alignSelf: 'center',
-    color: '#1C58B2',
-  },
-  create: {
-    fontSize: 18,
-    alignSelf: 'center',
-    color: '#1C58B2',
-  },
-});
+function Profile() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Profile!</Text>
+    </View>
+  );
+}
+
+function Notifications() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Notifications!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function NewsScreen() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Feed"
+      screenOptions={{
+        tabBarActiveTintColor: '#0891b2',
+      }}>
+      <Tab.Screen
+        name="Feed"
+        component={Feed}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={Notifications}
+        options={{
+          tabBarLabel: 'Updates',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="bell" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 export default NewsScreen;
